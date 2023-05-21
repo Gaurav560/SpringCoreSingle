9fa0c431ae9ae7eb2eb616a102000ecdd8e69bb3
GAURAV SHARMA <gsharma1154@gmail.com>
2023-05-21 10:48:52 +0530

    ref attribute

---
+++ springcore/src/main/java/com/springcore/ref/Test1.java
@@ -0,0 +1,14 @@
+package com.springcore.ref;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+ApplicationContext context=new ClassPathXmlApplicationContext("com/springcore/ref/refConfig.xml");
+A a=(A) context.getBean("aref");
+System.out.println(a);
+	}
+
+}
